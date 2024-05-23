11111992c719b3abafa5b7886eb12a94953b51a7
Kran <kran.olivares.rojas@gmail.com>
2024-05-23 15:45:58 -0400

    Abrir archivo y print por sección

--- funciones.hpp
+++ funciones.hpp
@@ -1,3 +1,6 @@
+#include <string>
+using namespace std;
+
 class super_string {
     private:
         struct nodo {
@@ -5,12 +8,14 @@ class super_string {
             int index;
             char c;
             nodo(int index, char c) {}
+            nodo(){}
         };
         int height = 0;       // Altura del árbol
         int length = 0;       // Largo del super-string
         nodo *root = nullptr; // Raíz del super-string
     public:
         super_string() {}
+        ~super_string() {}
         void juntar(super_string &s);
         void agregar(char c); // Insertar un caracter en la última posición
         // En la izquierda esta el super_string a y en la derecha el super_string b
@@ -20,107 +25,24 @@ class super_string {
         string stringizar(); // Debe ser O(n)
         void limpiar(); // Se deben borrar todos los nodos del super-string
 };
-
-super_string::super_string() {
-    root = new nodo(0, '$');
+/*
+void super_string::agregar(char c) {
 }
 
 void super_string::juntar(super_string &s) {
-    nodo *nuevo = new nodo(length, '$');
-    nuevo->left = root;
-    nuevo->right = s.root;
-    root = nuevo;
-    length += s.length;
-    height = max(height, s.height) + 1;
-}
-
-void super_string::agregar(char c) {
-    nodo *nuevo = new nodo(length, c);
-    nuevo->left = root;
-    root = nuevo;
-    length++;
-    height++;
 }
 
 void super_string::separar(int i, super_string &a, super_string &b) {
-    nodo *nodo_i = root;
-    while (nodo_i->left->index != i) {
-        if (nodo_i->left->index > i) {
-            nodo_i = nodo_i->left;
-        } else {
-            nodo_i = nodo_i->right;
-            i -= nodo_i->left->index + 1;
-        }
-    }
-    a.root = nodo_i->left;
-    b.root = nodo_i->right;
-    a.length = i;
-    b.length = nodo_i->left->index - i;
-    a.height = recortar();
-    b.height = nodo_i->left->height;
 }
 
 void super_string::reverso() {
-    stack<nodo*> pila;
-    pila.push(root);
-    while (!pila.empty()) {
-        nodo *nodo_actual = pila.top();
-        pila.pop();
-        swap(nodo_actual->left, nodo_actual->right);
-        if (nodo_actual->right != nullptr) {
-            pila.push(nodo_actual->right);
-        }
-        if (nodo_actual->left != nullptr) {
-            pila.push(nodo_actual->left);
-        }
-    }
 }
 
 int super_string::recortar() {
-    if (root->left->height == height) {
-        nodo *nuevo = root->left;
-        root = root->left;
-        delete nuevo;
-        height--;
-    }
-    return height;
 }
 
 string super_string::stringizar() {
-    string s;
-    stack<nodo*> pila;
-    pila.push(root);
-    while (!pila.empty()) {
-        nodo *nodo_actual = pila.top();
-        pila.pop();
-        if (nodo_actual->c != '$') {
-            s += nodo_actual->c;
-        }
-        if (nodo_actual->right != nullptr) {
-            pila.push(nodo_actual->right);
-        }
-        if (nodo_actual->left != nullptr) {
-            pila.push(nodo_actual->left);
-        }
-    }
-    return s;
 }
 
 void limpiar() {
-    stack<nodo*> pila;
-    pila.push(root);
-    while (!pila.empty()) {
-        nodo *nodo_actual = pila.top();
-        pila.pop();
-        if (nodo_actual->right != nullptr) {
-            pila.push(nodo_actual->right);
-        }
-        if (nodo_actual->left != nullptr) {
-            pila.push(nodo_actual->left);
-        }
-        delete nodo_actual;
-    }
-    root = nullptr;
-    height = 0;
-    length = 0;
-}
+}*/
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "funciones.hpp"
 using namespace std;
-//#include "funciones.hpp"
 
 int main() {
     ifstream fp;
-    string line;
+    string linea, oracion;
+    super_string super_string;
+    int pos_trabajo, pos_inicial, pos_final;
     fp.open("Prueba_1.txt");
 
     if (!fp.is_open()) {
@@ -14,11 +16,34 @@ int main() {
         return 1;
     }
 
-    fp >> line;
-    
-    while (line != "FIN") {
-        cout << line << endl;
-        fp >> line;
-    }
-}
+    fp >> linea;
 
+    while (linea != "FIN") {
+        if (linea == "INSERTAR") {
+            fp >> pos_trabajo;
+            fp >> linea;
+            cout << "[" << pos_trabajo << "," << linea <<"]" << endl;
+            //for (int i = 0; i < linea.size(); i++) {
+                //super_string.agregar(linea[i]);
+            //}
+        } else if (linea == "MOSTRAR") {
+            //oracion = super_string.stringizar();
+            cout << oracion << endl;
+        } else if (linea == "ELIMINAR") {
+            fp >> pos_inicial;
+            fp >> pos_final;
+            cout << "[" << pos_inicial << "," << pos_final <<"]" << endl;
+            //super_string.eliminar();
+        } else if (linea == "REVERSO") {
+            fp >> pos_inicial;
+            fp >> pos_final;
+            cout << "[" << pos_inicial << "," << pos_final <<"]" << endl;
+            //super_string.reverso();
+        } else if (linea == "RECORTAR") {
+            //super_string.recortar();
+        }
+        fp >> linea;
+    }
+
+    fp.close();
+}
