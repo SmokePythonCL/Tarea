b6600e06499ffbfe38b0efaf9ff5b2ce6f36fba0
Kran <kran.olivares.rojas@gmail.com>
2024-06-01 21:54:09 -0400

    Comentarios

--- TDA_SuperString.hpp
+++ TDA_SuperString.hpp
@@ -33,6 +33,16 @@ class super_string {
         int Altura(nodo* node);
 };
 
+/*****
+* void super_string::agregar
+******
+* Agrega un nodo a la derecha del último nodo del super-string
+******
+* Input:
+* char letra: Caracter a agregar
+******
+* Returns:
+*****/
 void super_string::agregar(char letra) {
     if (root == nullptr) {
         root = new nodo(0, letra);
@@ -49,13 +59,34 @@ void super_string::agregar(char letra) {
     }
 }
 
-
+/*****
+* string super_string::stringizar
+******
+* Con la ayuda de inOrderStringizar, retorna un string con todos los caracteres del super-string en recorrido de inOrder
+******
+* Input:
+******
+* Returns:
+* string, string con todos los caracteres del super-string
+*****/
 string super_string::stringizar() {
     string oracion;
     inOrderStringizar(root, oracion);
     return oracion;
 }
 
+/*****
+* void super_string::separar
+******
+* Separa el super-string en dos super-strings a y b, donde a tiene los primeros i caracteres y b el resto
+******
+* Input:
+* int i: Cantidad de caracteres que tendrá a
+* super_string &a: Super-string que será llenado con los primeros i caracteres
+* super_string &b: Super-string que será llenado con el resto de caracteres
+******
+* Returns:
+*****/
 void super_string::separar(int i, super_string &a, super_string &b) {
     super_string temp;
 
@@ -69,6 +100,15 @@ void super_string::separar(int i, super_string &a, super_string &b) {
     }
 }
 
+/*****
+* void super_string::limpiar
+******
+* Elimina todos los nodos del super-string, generando un super-string vacío
+******
+* Input:
+******
+* Returns:
+*****/
 void super_string::limpiar() {
     if (root != nullptr) {
         nodo *temp = root;
@@ -84,6 +124,16 @@ void super_string::limpiar() {
     height = 0;
 }
 
+/*****
+* void super_string::juntar
+******
+* Inserta el super-string s al final del super-string actual
+******
+* Input:
+* super_string &s: Super-string que se insertará
+******
+* Returns:
+*****/
 void super_string::juntar(super_string &s) {
     if (s.root != nullptr) {
         if (length == 0) {
@@ -105,6 +155,15 @@ void super_string::juntar(super_string &s) {
     }
 }
 
+/*****
+* void super_string::reverso
+******
+* Función que con la ayuda de inOrderReverso invierte el super-string actual
+******
+* Input:
+******
+* Returns:
+*****/
 void super_string::reverso() {
     super_string temp;
     inOrderReverso(root, temp);
@@ -113,6 +172,16 @@ void super_string::reverso() {
     length = temp.length;
 }
 
+/*****
+* int super_string::recortar
+******
+* Ayuda a disminuir el tiempo de búsqueda de un nodo en el árbol desde uno O(n) a O(log n)
+******
+* Input:
+******
+* Returns:
+* int, altura del árbol después de recortar
+*****/
 int super_string::recortar() {
     nodo** nodos = new nodo*[length];
     int i = 0;
@@ -124,6 +193,17 @@ int super_string::recortar() {
     return height;
 }
 
+/*****
+* void super_string::inOrderStringizar
+******
+* Realiza un recorrido inOrder del árbol y va concatenando los caracteres en un string
+******
+* Input:
+* nodo* node: Nodo actual
+* string& oracion: String que se va concatenando
+******
+* Returns:
+*****/
 void super_string::inOrderStringizar(nodo* node, string& oracion) {
     if (node != nullptr) {
         inOrderStringizar(node->left, oracion);
@@ -132,6 +212,19 @@ void super_string::inOrderStringizar(nodo* node, string& oracion) {
     }
 }
 
+/*****
+* void super_string::inOrderSeparar
+******
+* Realiza un recorrido inOrder del árbol y va separando los caracteres en dos super-strings a y b, donde a tiene los primeros i caracteres y b el resto.
+******
+* Input:
+* nodo* node: Nodo actual
+* super_string& a: Super-string que se va llenando con los primeros i caracteres
+* super_string& b: Super-string que se va llenando con el resto de caracteres
+* int i: Cantidad de caracteres que tendrá a
+******
+* Returns:
+*****/
 void super_string::inOrderSeparar(nodo* node, super_string& a, super_string& b, int i) {
     if (node != nullptr) {
         inOrderSeparar(node->left, a, b, i);
@@ -144,6 +237,17 @@ void super_string::inOrderSeparar(nodo* node, super_string& a, super_string& b, 
     }
 }
 
+/*****
+* void super_string::inOrderReverso
+******
+* Realiza un recorrido inOrder de forma inversa (De derecha a izquierda) del árbol y va concatenando los caracteres en un super-string
+******
+* Input:
+* nodo* node: Nodo actual
+* super_string& temp: Super-string que se va llenando con los caracteres en orden inverso
+******
+* Returns:
+*****/
 void super_string::inOrderReverso(nodo* node, super_string& temp) {
     if (node != nullptr) {
         inOrderReverso(node->right, temp);
@@ -152,6 +256,18 @@ void super_string::inOrderReverso(nodo* node, super_string& temp) {
     }
 }
 
+/*****
+* void super_string::inOrderTransversal
+******
+* Recolecta los nodos del árbol en un arreglo de nodos mediante un recorrido inOrder
+******
+* Input:
+* nodo* node: Nodo actual
+* nodo** nodos: Arreglo de nodos
+* int& i: Índice del arreglo de nodos
+******
+* Returns:
+*****/
 void super_string::inOrderTransversal(nodo* node, nodo** nodos, int& i) {
     if (!node) return;
 
@@ -160,6 +276,19 @@ void super_string::inOrderTransversal(nodo* node, nodo** nodos, int& i) {
     inOrderTransversal(node->right, nodos, i);
 }
 
+/*****
+* super_string::nodo* super_string::Balancear
+******
+* Recorre el arbol desde la mitad y va agregando los nodos de tal forma que el arbol queda balanceado
+******
+* Input:
+* nodo** nodos: Arreglo de nodos
+* int inicio: Inicio del arreglo
+* int fin: Fin del arreglo
+******
+* Returns:
+* nodo*, Retorna un nodo con sus hijos balanceados
+*****/
 super_string::nodo* super_string::Balancear(nodo** nodos, int inicio, int fin) {
     if (inicio > fin) return nullptr;
 
@@ -172,6 +301,17 @@ super_string::nodo* super_string::Balancear(nodo** nodos, int inicio, int fin) {
     return node;
 }
 
+/*****
+* int super_string::Altura
+******
+* Retorna la altura del árbol
+******
+* Input:
+* nodo* node: Nodo actual
+******
+* Returns:
+* int, Altura maxima de sus hijos más su propia altura (+1)
+*****/
 int super_string::Altura(nodo* node) {
     if (!node) return 0;
     return 1 + max(Altura(node->left), Altura(node->right));
--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@
 #include "TDA_SuperString.hpp"
 using namespace std;
 
-void insertar(super_string &super_string_principal, int pos_trabajo, string linea) {
+/*****
+* void Insertar
+******
+* Mediante un recorrido del string entregado, va agregando los caracteres al super_string
+******
+* Input:
+* super_string &super_string_principal: Super_string al que se le agregará la línea
+* int pos_trabajo: Posición en la que se insertará la línea
+* string linea: String que se insertará en el super_string
+******
+* Returns:
+*****/
+void Insertar(super_string &super_string_principal, int pos_trabajo, string linea) {
     super_string super_string_2;
 
     super_string_principal.separar(pos_trabajo, super_string_principal, super_string_principal);
@@ -15,7 +27,19 @@ void insertar(super_string &super_string_principal, int pos_trabajo, string line
     super_string_principal.juntar(super_string_2);
 }
 
-void reverso(super_string &super_string_principal, int pos_inicial, int pos_final) {
+/*****
+* void Reverso
+******
+* Cambia el orden de los caracteres en el super_string invirtiendolo entre las posiciones pos_inicial y pos_final
+******
+* Input:
+* super_string &super_string_principal: Super_string al que se le invertirá el orden de los caracteres
+* int pos_inicial: Posición inicial de la inversión
+* int pos_final: Posición final de la inversión
+******
+* Returns:
+*****/
+void Reverso(super_string &super_string_principal, int pos_inicial, int pos_final) {
     super_string super_string_2, super_string_3;
     super_string_principal.separar(pos_inicial, super_string_principal, super_string_2);
     super_string_2.separar(pos_final - pos_inicial + 1, super_string_2, super_string_3);
@@ -24,7 +48,19 @@ void reverso(super_string &super_string_principal, int pos_inicial, int pos_fina
     super_string_principal.juntar(super_string_3);
 }
 
-void eliminar(super_string &super_string_principal, int pos_inicial, int pos_final) {
+/*****
+* void Eliminar
+******
+* Elimina los caracteres en el rango de las posiciones pos_inicial y pos_final
+******
+* Input:
+* super_string &super_string_principal: Super_string al que se le eliminarán los caracteres
+* int pos_inicial: Posición inicial de la eliminación
+* int pos_final: Posición final de la eliminación
+******
+* Returns:
+*****/
+void Eliminar(super_string &super_string_principal, int pos_inicial, int pos_final) {
     super_string super_string_2, super_string_3;
 
     super_string_principal.separar(pos_inicial, super_string_principal, super_string_2);
@@ -32,6 +68,16 @@ void eliminar(super_string &super_string_principal, int pos_inicial, int pos_fin
     super_string_principal.juntar(super_string_3);
 }
 
+/*****
+* int main
+******
+* Lee un archivo de texto con instrucciones, y a medida que las lee, realiza las operaciones correspondientes en un super_string
+******
+* Input:
+******
+* Returns:
+* int:
+*****/
 int main() {
     ifstream fp;
     string linea, oracion;
@@ -50,18 +96,18 @@ int main() {
         if (linea == "INSERTAR") {
             fp >> pos_trabajo;
             fp >> linea;
-            insertar(super_string_principal, pos_trabajo, linea);
+            Insertar(super_string_principal, pos_trabajo, linea);
         } else if (linea == "MOSTRAR") {
             oracion = super_string_principal.stringizar();
             cout << oracion << endl;
         } else if (linea == "ELIMINAR") {
             fp >> pos_inicial;
             fp >> pos_final;
-            eliminar(super_string_principal, pos_inicial, pos_final);
+            Eliminar(super_string_principal, pos_inicial, pos_final);
         } else if (linea == "REVERSO") {
             fp >> pos_inicial;
             fp >> pos_final;
-            reverso(super_string_principal, pos_inicial, pos_final);
+            Reverso(super_string_principal, pos_inicial, pos_final);
         }else if (linea == "RECORTAR") {
             cout << super_string_principal.recortar() << endl;
         }
